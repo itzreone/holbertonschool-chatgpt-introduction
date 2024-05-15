9a1a5d48d2ef3a5bbf3c31464fa60b6a36ea803e
Ravan <itzreone@gmail.com>
2024-05-15 19:41:12 +0400

    upd

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,12 +8,3 @@ def factorial(n):
 
 f = factorial(int(sys.argv[1]))
 print(f)
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1  # Decrement n in each iteration
-    return result
-
-f = factorial(int(sys.argv[1]))
-print(f)
